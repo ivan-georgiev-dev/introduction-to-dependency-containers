1942f82a6e8099dccfeb8abd07b775ee3357bbc9
Ivan Georgiev <contact@ivangeorgiev.dev>
2025-03-10 00:09:59 +0100

    Add support for singletons

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 function createDependencyContainer() {
   const registrations = {};
 
-  function register(id, dependencies, factory) {
+  function register(id, dependencies, factory, singleton = true) {
     registrations[id] = {
       id,
       dependencies,
       factory,
+      singleton,
+      instance: undefined,
     };
   }
 
@@ -16,17 +18,25 @@ function createDependencyContainer() {
       throw new Error('No registration with ID ' + id + ' found.');
     }
 
-    const { dependencies, factory } = registration;
+    const { singleton, instance } = registration;
 
-    if (dependencies.length === 0) {
-      return factory();
+    if (singleton && instance) {
+      return instance;
     }
 
-    const resolvedDependencies = dependencies.map((dependency) =>
-      resolve(dependency),
-    );
+    const { dependencies, factory } = registration;
+    const resolvedDependencies =
+      dependencies.length === 0
+        ? []
+        : dependencies.map((dependency) => resolve(dependency));
 
-    return factory(resolvedDependencies);
+    const newInstance = factory(resolvedDependencies);
+
+    if (singleton) {
+      registration.instance = newInstance;
+    }
+
+    return newInstance;
   }
 
   return {
@@ -34,66 +44,3 @@ function createDependencyContainer() {
     resolve,
   };
 }
-
-class TodoApiService {
-  constructor(baseUrl) {
-    this.baseUrl = baseUrl;
-  }
-
-  async getTodo(id) {
-    const response = await fetch(`${this.baseUrl}/todos/${id}`);
-
-    const todo = await response.json();
-
-    return todo;
-  }
-}
-
-class TodoService {
-  constructor(todoApiService, todoFormatter) {
-    this.todoApiService = todoApiService;
-    this.todoFormatter = todoFormatter;
-  }
-
-  async getFormattedTodos(ids) {
-    const todos = await Promise.all(
-      ids.map((id) => this.todoApiService.getTodo(id)),
-    );
-
-    return todos.map((todo) => this.todoFormatter.format(todo));
-  }
-}
-
-class TodoFormatter {
-  format(todo) {
-    return todo.title + ' - ' + (todo.completed ? 'done.' : 'not done.');
-  }
-}
-
-const dependencyContainer = createDependencyContainer();
-
-dependencyContainer.register(
-  'baseUrl',
-  [],
-  () => 'https://jsonplaceholder.typicode.com',
-);
-
-dependencyContainer.register(
-  'TodoApiService',
-  ['baseUrl'],
-  ([baseUrl]) => new TodoApiService(baseUrl),
-);
-
-dependencyContainer.register('TodoFormatter', [], () => new TodoFormatter());
-
-dependencyContainer.register(
-  'TodoService',
-  ['TodoApiService', 'TodoFormatter'],
-  ([todoApiService, todoFormatter]) =>
-    new TodoService(todoApiService, todoFormatter),
-);
-
-const todoService = dependencyContainer.resolve('TodoService');
-todoService
-  .getFormattedTodos([1, 2, 3])
-  .then((formattedTodos) => console.log(formattedTodos));
